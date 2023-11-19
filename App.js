cbe174e538f785f0c2430e0d73351202d3ff19c9
Sanketh Bennur <snkthbnnr3@gmail.com>
2023-11-20 00:25:02 +0530

    Components: Got my first component to work!  - Used multiple props  - Props are defined in {} braces  - They are called like regular HTML Tag properties

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import FoodCard from './src/components/common/FoodCard';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App()
+{
+	return (
+		<View style={ styles.container }>
+			<Text>My food delivery app!</Text>
+			<FoodCard
+				image_url="https://www.bhg.com/thmb/L42rOmj0pORpp2WwEwwXSltF-X4=/1244x0/filters:no_upscale():strip_icc()/thin-crust-pepperoni-vegetable-pizza-RU207102-9d5b6151a9074ec18a7af235fb332d83.jpg"
+				title="Thin Crust Vegetable Pizza"
+				description="Piled with flavorful veggies like peppers and artichokes, this vegetarian pizza will be a hit at your next pizza night! Fresh basil takes it over the top."
+				price="7.0"
+			/>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ src/components/common/FoodCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from "react-native";
+
+function FoodCard({ image_url = "", title = "", description = "", price = 0.0 })
+{
+    return (
+        <View style={ style.card }>
+            <Image source={ { uri: image_url } } style={ style.card_image } />
+            <Text style={ style.card_title } >{ title }</Text>
+            <Text style={ style.card_description }>{ description }</Text>
+            <Text style={ style.card_price }>{ price }</Text>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 16,
+        margin: 8,
+        width: 150,
+        elevation: 4,
+    },
+    card_image: {
+        width: "100%",
+        height: "70%",
+        objectFit: "cover",
+        marginBottom: 8,
+        borderRadius: 8,
+    },
+    card_title: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 4,
+    },
+    card_description: {
+        fontSize: 12,
+        color: "#888",
+        marginBottom: 4,
+    },
+    card_price: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#3498db',
+    },
+});
+
+export default FoodCard;
