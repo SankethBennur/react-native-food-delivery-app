5859167cc78ed64baeda7b06f193a796a4bb2419
Sanketh Bennur <snkthbnnr3@gmail.com>
2023-11-20 13:25:55 +0530

    Debug: Resolved error with clipping in ScrollView

--- models/restaurants_menu.js
+++ models/restaurants_menu.js
@@ -5,38 +5,38 @@ const restaurant_list = [
         menu: [
             {
                 __id: "a001",
-                image: "",
+                image: "https://dinnerthendessert.com/wp-content/uploads/2019/11/BBQ-Chicken-Wings-4x3.jpg",
                 title: "Barbeque Chicken Wings",
-                description: "",
+                description: "BBQ Chicken Wings are an awesome game day appetizer, baked chicken wings smothered in barbecue sauce. Crispy inside and sweet and sticky on the outside!",
                 price: 10.0
             },
             {
                 __id: "a002",
-                image: "",
-                title: "",
-                description: "",
-                price: 10.0
+                image: "https://www.sargento.com/assets/Uploads/Recipe/Image/808-Chili_Cheese_Nachos__FillWzExNzAsNTgzXQ.jpg",
+                title: "Cheese Nachos",
+                description: "You’re sure to love this nacho recipe and so will your guests when you bring out this hot plate of tortilla chips smothered in meaty chili spiced with barbecue sauce and topped with green onions.",
+                price: 12.0
             },
             {
                 __id: "a003",
-                image: "",
-                title: "",
-                description: "",
-                price: 10.0
+                image: "https://hips.hearstapps.com/hmg-prod/images/martini-recipe-1657532229.jpg?crop=1.00xw:0.669xh;0,0.170xh&resize=2048:*",
+                title: "Martini",
+                description: "A classic martini compromises of just TWO ingredients - gin or vodka, and vermouth. It's then often garnished with an olive, or lemon peel (or both).",
+                price: 14.0
             },
             {
                 __id: "a004",
-                image: "",
-                title: "",
-                description: "",
-                price: 10.0
+                image: "https://www.moodymixologist.com/wp-content/uploads/2020/02/frozen-margarita-cocktail-recipe-02-7384996.jpg.webp",
+                title: "Margareita",
+                description: "The classic Frozen Margarita is one of the best ways to cool off on a hot summer day. It’s also incredibly easy to blend up any time you’re in the mood for a sweet and sour tequila slushie!",
+                price: 16.0
             },
             {
                 __id: "a005",
-                image: "",
-                title: "",
-                description: "",
-                price: 10.0
+                image: "https://assets.bonappetit.com/photos/57acd021f1c801a1038bc7b7/1:1/w_2560%2Cc_limit/steamed-japanese-rice.jpg",
+                title: "Steamed rice",
+                description: "In Japanese, the word for food is the same as the one for rice. Without it, a meal is not a meal. The real genius of rice is revealed when you think about it in reverse: Add just about anything to a bowl of it and, voilà! you’ve got dinner.",
+                price: 18.0
             },
         ],
     },
@@ -46,9 +46,9 @@ const restaurant_list = [
         menu: [
             {
                 __id: "b001",
-                image: "",
-                title: "",
-                description: "",
+                image: "https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/awesome-american-cheesy.17f198e5a73db40c367ddfe96da54a4c.1.jpg",
+                title: "Awesome American Cheesy",
+                description: "Our divine peruvian flavoured cheesy sauce, topped with sweet corn, green capsicum, red paprika and 100% mozzarella cheese, finished with a generous drizzle of texas garlic sauce.",
                 price: 10.0
             },
             {
--- src/components/Home/RestaurantList.js
+++ src/components/Home/RestaurantList.js
@@ -1,97 +1,52 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
-import { Animated } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    ScrollView
+} from "react-native";
+
 import FoodCard from "../common/FoodCard";
 
 const __restaurant_list = require("../../../models/restaurants_menu");
 
-const RestaurantList = () =>
-{
-    const [scrollViewWidth, setScrollViewWidth] = React.useState(0);
-    const boxWidth = scrollViewWidth * 0.8;
-    const boxDistance = scrollViewWidth - boxWidth;
-    const halfBoxDistance = boxDistance / 2;
-    const pan = React.useRef(new Animated.ValueXY()).current;
-
+const RestaurantList = () => {
     return (
-        <View style={ styles.container }>
-            <FlatList
-                data={ __restaurant_list }
-                keyExtractor={ (item) => item.__id }
-                renderItem={ ({ item }) => (
-                    <View>
-                        <Text style={ styles.restaurantName }>{ item.name }</Text>
-                        <FlatList
+        <View>
+            <ScrollView style={ styles.container }>
+                { __restaurant_list.map((res_item) => (
+                    <View key={ res_item.__id }>
+                        <Text style={ styles.restaurant_name }>{ res_item.name }</Text>
+                        <ScrollView
                             horizontal
-                            data={ item.menu }
-
-                            contentContainerStyle={ { paddingVertical: 16 } }
-                            contentInsetAdjustmentBehavior="never"
-                            snapToAlignment="center"
-                            decelerationRate="fast"
-                            automaticallyAdjustContentInsets={ false }
                             showsHorizontalScrollIndicator={ false }
-                            showsVerticalScrollIndicator={ false }
-                            // scrollEventThrottle={ 1 }
-                            snapToInterval={ boxWidth }
-                            contentInset={ {
-                                left: halfBoxDistance,
-                                right: halfBoxDistance,
-                            } }
-                            contentOffset={ { x: halfBoxDistance * -1, y: 0 } }
-                            onLayout={ (e) =>
-                            {
-                                setScrollViewWidth(e.nativeEvent.layout.width);
-                            } }
-                            onScroll={ Animated.event(
-                                [{ nativeEvent: { contentOffset: { x: pan.x } } }],
-                                {
-                                    useNativeDriver: false,
-                                },
-                            ) }
-
-                            keyExtractor={ (item) => item.__id }
-                            renderItem={ ({ item, index }) => (
-                                <Animated.View
-                                    style={ {
-                                        transform: [
-                                            {
-                                                scale: pan.x.interpolate({
-                                                    inputRange: [
-                                                        (index - 1) * boxWidth - halfBoxDistance,
-                                                        index * boxWidth - halfBoxDistance,
-                                                        (index + 1) * boxWidth - halfBoxDistance, // adjust positioning
-                                                    ],
-                                                    outputRange: [0.8, 1, 0.8], // scale down when out of scope
-                                                    extrapolate: 'clamp',
-                                                }),
-                                            },
-                                        ],
-                                    } }>
-
+                        >
+                            { res_item.menu.map((food_item) => (
+                                <View key={ food_item.__id }>
+                                    {/* <Text>{ food_item.title }</Text> */ }
                                     <FoodCard
-                                        image_url="https://cdn.loveandlemons.com/wp-content/uploads/2023/02/vegetarian-pizza-500x500.jpg"
-                                        title="Title"
-                                        description="food_item.description"
-                                        price={ 1.0 }
+                                        image_url={ food_item.image }
+                                        title={ food_item.title }
+                                        description={ food_item.description }
+                                        price={ food_item.price }
                                     />
-
-                                </Animated.View>
-                            ) }
-                        />
+                                </View>
+                            )) }
+                        </ScrollView>
                     </View>
-                ) }
-            />
+                )) }
+            </ScrollView>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        width: "100vw",
         flex: 1,
         padding: 16,
     },
-    restaurantName: {
+    restaurant_name: {
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 8,
